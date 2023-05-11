4510086898ee070947f48ffd78f05cb2fe801564
“candymintxz21” <sudarat.pu64@rsu.ac.th>
2023-05-11 21:57:49 +0700

    1

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 var express = require('express')
 var cors = require('cors')
-
+// var bodyParser = require('body-parser')
+// var jsonParser = bodyParser.json() 
 var jwt = require('jsonwebtoken');
 const secret = 'Lilyn'
 
-//const mysql = require('mysql2');
-/* const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    database: 'webb'
-}); */
+// const mysql = require('mysql2');
+//  const connection = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     database: 'webb'
+// }); 
 
 require('dotenv').config()
 const mysql = require('mysql2')
@@ -84,19 +85,19 @@ app.get('/information/:id', function (req, res, next) {
 })
 
 app.post('/addinformation', function (req, res, next) {
-    const data1 = req.body.data1;
-    const data2 = req.body.data2;
-    const data3 = req.body.data3;
-    const data4 = req.body.data4;
-    const data5 = req.body.data5;
-    const data6 = req.body.data6;
-    const data7 = req.body.data7;
-    const data8 = req.body.data8;
-    const data9 = req.body.data9;
-    const data10 = req.body.data10;
+    // const data1 = req.body.data1;
+    // const data2 = req.body.data2;
+    // const data3 = req.body.data3;
+    // const data4 = req.body.data4;
+    // const data5 = req.body.data5;
+    // const data6 = req.body.data6;
+    // const data7 = req.body.data7;
+    // const data8 = req.body.data8;
+    // const data9 = req.body.data9;
+    // const data10 = req.body.data10;
     connection.execute(
-        'INSERT INTO information (`cardnumber`, `name`, `surname`, `nickname`, `age`, `address`, `parentname`, `phone`, `email`, `allergicfood`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
-        [req.body.data1, req.body.data2, req.body.data3, req.body.data4, req.body.data5, req.body.data6, req.body.data7, req.body.data8, req.body.data9, req.body.data10],
+        'INSERT INTO information (cardnumber, name, surname, nickname, age, address, parentname, phone, email, allergicfood) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
+        [req.body.cardnumber, req.body.name, req.body.surname, req.body.nickname, req.body.age, req.body.address, req.body.parentname, req.body.phone, req.body.email, req.body.allergicfood],
         function (err, results) {
             if (err) {
                 console.log(err);
@@ -110,7 +111,7 @@ app.post('/addinformation', function (req, res, next) {
 app.post('/login' , function (req, res, next) {
     connection.execute(
         'SELECT * FROM information WHERE email = ? AND cardnumber = ?',
-        [req.body.Users,req.body.Password],
+        [req.body.email,req.body.cardnumber],
         function(err, results, fields) {
             if (err) {
                 res.json({status: 'error', message: err})
